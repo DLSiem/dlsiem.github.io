a7f49686cf47ce06eb828d93cdc62de66c50df7b
DLSiem <pdlsiem17@gmail.com>
2023-11-18 13:49:51 +0530

    Add es tt

--- Index.js
+++ Index.js
@@ -37,3 +37,18 @@ setInterval(updateClock, 1000);
 
 // Initial call to update the clock
 updateClock();
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
