915566791a1b40ecca8cb3e3bb00fb3b23003530
D L Siem <pdlsiem17@gmail.com>
2025-03-06 22:56:36 +0530

    adding back to top button

--- Index.js
+++ Index.js
@@ -52,3 +52,20 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// back to top button
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTop = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      backToTop.style.display = "flex";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+
+  backToTop.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
